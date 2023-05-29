b68090cfa93bae51c25229901172dde02da12331
djilit <ahmedoudjilit@gmail.com>
2023-05-29 13:37:17 +0200

    ORM setup

---
+++ models/loginTest.js
@@ -0,0 +1,31 @@
+// user.js
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config'); // Import the configured Sequelize instance
+
+const logintest = sequelize.define('logintest', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  reponse:{
+    type:DataTypes.JSON,
+  
+
+  },
+  repExcepte:{
+    type:DataTypes.JSON,
+    allowNull: false
+  }
+ 
+});
+
+module.exports = logintest;
